a84dfbd62ea79c3a0ef4ee3ed35d96136f97930d
ceomssweb <ceomssweb@gmail.com>
2022-05-21 04:57:38 +0530

    added documents link

--- bright-sun-info/src/app/home-page/home-page.component.ts
+++ bright-sun-info/src/app/home-page/home-page.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { Component, Inject, OnInit } from '@angular/core';
 
 @Component({
   selector: 'bs-home-page',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomePageComponent implements OnInit {
 
-  constructor() { }
+  constructor(@Inject(DOCUMENT) private document: Document) { }
 
   ngOnInit(): void {
   }
-
+  externalApps(){
+    window.open(
+      'https://brightsundocuments.com',
+      '_blank'
+    );
+  }
 }
